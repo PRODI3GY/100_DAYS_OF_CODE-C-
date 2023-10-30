78ef731835c83b7d0ed81839c3b0a30ed86632ef
PRODI3GY <102425181+PRODI3GY@users.noreply.github.com>
2023-10-30 13:17:29 +0300

    CREATED 2 FUNCTIONS:OBJECTIVE 4 Wrote 2 functions, one that take arguments and one that does not

---
+++ DAY-6/function1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int addition(int a, int b)
+{
+	int c = a + b ;
+	return c;
+}
+int main()
+{
+	printf("%d",addition(6,9));
+	return 0;
+}
---
+++ DAY-6/function3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void happy_birthday()
+{
+	printf("HAPPY BIRTHDAY\n");
+	printf("HAPPY BIRTHDAY\n");
+	printf("HAPPY BIRTHDAY\n");
+	printf("HAPPY BIRTHDAY\n");
+}
+
+int main()
+{
+        happy_birthday();
+	return 0;
+}
